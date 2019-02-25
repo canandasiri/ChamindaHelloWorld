d95ada950d45c613da75d1ad5176a2587bcf5d32
canandasiri <canandasiri@gmail.com>
2019-02-25 12:09:07 +0530

    Commented out the Accounts SVC

--- ChamindaHelloWorld/lambda.js
+++ ChamindaHelloWorld/lambda.js
@@ -15,18 +15,18 @@ exports.handler = function (event, context, callback) {
     }).then((response) => {
         var access_token = "bearer " + response.body.access_token;
         // Insert new API calls here to call with APIX Access Token
-        Swagger.http({
-            url: `https://api.apixplatform.com/sbaccount/1.0/AccountService/accounts/accounts`,
-            method: 'get',
-            query: { "page": "0", "size": "1" },
-            headers: { "X-Authorization": access_token, "Accept": "*/*" }
-        }).then((response) => {
-            console.log(response);
-            console.log("-----------Calling Account types-----------")
-            // your code goes here
-        }).catch((err) => {
+        //Swagger.http({
+        //    url: `https://api.apixplatform.com/sbaccount/1.0/AccountService/accounts/accounts`,
+        //    method: 'get',
+        //    query: { "page": "0", "size": "1" },
+        //    headers: { "X-Authorization": access_token, "Accept": "*/*" }
+        //}).then((response) => {
+        //    console.log(response);
+        //    console.log("-----------Calling Account types-----------")
+        //    // your code goes here
+        //}).catch((err) => {
             // error handling goes here
-        });
+        //});
         Swagger.http({
             url: `https://api.apixplatform.com/sbaccount/1.0/AccountService/accounts/account-types`,
             method: 'get',
@@ -38,12 +38,9 @@ exports.handler = function (event, context, callback) {
         }).catch((err) => {
             // error handling goes here
         });
-
-
         callback(null, access_token);
     }).catch((err) => {
         console.log(err);
         callback("Execution failed");
     });
-
 }
